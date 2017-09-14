eacd35d3a399969045135ea64d96e3593dcbe1f0
Sean Hayes <seanhayes13@hotmail.com>
2017-09-14 15:00:50 -0600

    Create helloWorld.cs

---
+++ CSSamples/helloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjectMarathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	Console.WriteLine("Hello World");
+        }
+    }
+}
