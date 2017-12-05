0952241b3576b5fbd769da39ea63672ea3c616cf
Sean Hayes <seanhayes13@hotmail.com>
2017-12-05 14:00:37 -0700

    201712051400

---
+++ CPPSamples/sampleExternalFile.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+/*Filler text representing more code*/
+
+void sayGoodByeFromExternal() {
+	cout << "Good bye from external" << endl;
+}
---
+++ CPPSamples/splitFile.cpp
@@ -0,0 +1,17 @@
+/*Better to start by building the header file then work on this file.
+In the .cpp file you will need to include the header file that contains the
+method prototypes that you will be defining.*/
+
+#include "stdafx.h"
+#include "splitFile.h"
+#include <iostream>
+
+using namespace std;
+
+/*Now define the methods*/
+
+void helloFromSplitFile() {
+	cout << "Hello from split files" << endl;
+}
+
+/*Head back to the cppBookSamples.cpp file*/
