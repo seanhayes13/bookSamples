30f5da4ba2688d83dc6eb57cf39be817d57f0de0
Sean Hayes <seanhayes13@hotmail.com>
2017-12-05 14:12:09 -0700

    201712051412

---
+++ CPPSamples/main/splitFile.h
@@ -0,0 +1,12 @@
+/*This file should be written first before it's .cpp companion. You will
+need to include any other header files (more on this when we go over
+inheritance) and at a minimum the "using namespace std" line.*/
+
+#include "stdafx.h"
+
+using namespace std;
+
+/*Now list only the prototypes of all the methods that you will be defining
+in the .cpp file.*/
+
+void helloFromSplitFile();
