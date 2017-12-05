f617e2ebb1ad8e5606d1e69a8325449927681c73
Sean Hayes <seanhayes13@hotmail.com>
2017-12-05 14:05:33 -0700

    201712051405

---
+++ CPPSamples/multipleFilesStart.cpp
@@ -0,0 +1,24 @@
+// multipleFilesStart.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	/*This is the starting file for this section. It only has comments
+	listing what we need to do and where.*/
+
+	/*Here in the main we will create a menu of options for the user. This menu
+	will be a list of known 'applications' that we will program. The user will
+	then make a choice, the correct external file and method will be called and
+	run, then return the user to the menu. This continues until the user exits*/
+
+	/*First you need a way to 'loop' through bringing up the menu, getting a choice
+	from the user, then going back to the menu. The menu may start out small but is
+	likely going to grow. Will you want to write a cout statement for a dozen or
+	more options?*/
+
+    return 0;
+}
+
