7a8f0a775c1a67f46894ba58689de6836b6a22ae
Sean Hayes <seanhayes13@hotmail.com>
2017-12-21 13:07:16 -0700

    Create typecasting_part2.cpp

---
+++ CPPSamples/TypeCastingPart2/typecasting_part2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "genericLaptop.h"
+
+using namespace std;
+
+void main() {
+	GenericLaptop g1 = GenericLaptop();
+	cout << "Generic Laptop Name: " << g1.name << endl;
+
+	GenericLaptop g2 = GenericLaptop();
+	//WIP
+
+	system("pause");
+}
